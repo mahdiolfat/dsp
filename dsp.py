400cd6560a4d4e6b501b5dea4ce5be9dc063040c
mahdi <mahdi@adhawkmicrosystems.com>
2020-07-06 23:27:30 -0400

    pisarenko frequency estimation implemented

--- dsp.py
+++ dsp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
 import scipy.signal
+import scipy.linalg
 import matplotlib.pyplot as plt
 
 class dsp:
@@ -18,31 +19,46 @@ def pisarenko(signal, order, autocorrelation=None):
     Psarenko's method for frequency estimation
     Process is assumed to consist of order complex exponentials in white noise
 
-    signal with additive white noise
-    '''
-    if autocorrelation:
-        if len(autocorrelation) != order + 1:
-            raise ValueError("Pisarenki: autocorrelation sequence MUST be the size of order + 1")
-        # TODO: estimate autocorrelation here
+    signal: signal with additive white noise
 
-    # estimate the (order + 1) x (order + 1) autocorrelation matrix
-    autocorrelation = dsp.autocorrelation(signal)
+    TODO:
+        - use covariance
+        - handle 0-meaning iput data, or just calculate the covariance
+    '''
+    if not autocorrelation:
+        # estimate the (order + 1) x (order + 1) autocorrelation matrix
+        autocorrelation = dsp.autocorrelation(signal)
+
+    # autocorrelation sequence MUST be of size > order + 1
+    # only the first (order + 1) of the autocorrelation sequence is used
     rx = np.array(autocorrelation[:order + 1])
 
-    # TODO: 
-    autocorrelation_matrix = sp.linalg.toeplitz(rx)
+    # (order + 1) x (order + 1) hermitian toeplitz auttocorrelation matrix
+    autocorrelation_matrix = scipy.linalg.toeplitz(rx)
     print(autocorrelation_matrix)
 
+    # since autocorrelation matrix is hermitian toeplitz, eigenvalues are non-negative real
     eval, evec = np.linalg.eigh(autocorrelation_matrix)
     print(f'Eigen values: {eval}')
-    sigma = {np.min(eval)}
-    print(f'Min eigenval: {sigma}')
     print("Eigen vectors: ", evec)
 
     # there is only one noise vector
+    # it is the column vector corresponding to the minimum eigenvalue
+    # the minimum eigenvalue absolute value is the variance of the additive white noise
+    vmin_i = np.argmin(eval)
+    variance = eval[vmin_i]
+    print(f'Min eigenval (variance of white noise): {variance}')
+    # noise vector
+    vmin = evec[:,vmin_i]
+    eigenfilter = scipy.signal.tf2zpk([1], vmin)
+
+    # estimated frequencies
+    freqs = np.angle(eigenfilter[1])
+
     # there are (order) signal vectors
-    # TODO: find the minimum eigenvalue and the corresponding (p + 1) x (p + 1) auttocorrelation
-    # matrix Rx
+    # TODO: calculate power associated with each signal vector
+
+    return freqs, variance
 
 if __name__ == "__main__":
     SAMPLE_COUNT = 10000
@@ -50,7 +66,10 @@ if __name__ == "__main__":
     noise = np.random.normal(0, 0.1, SAMPLE_COUNT)
     sig = np.sin(space * 2*np.pi) + noise
 
-    pisarenko(sig, 1)
+    freqs, variance = pisarenko(sig, 1)
+
+    print(f'Frequencies: {freqs}')
+    print(f'Variance: {variance}')
     #ps = dsp.powerspectrum(sig)
     #peaks, _ = scipy.signal.find_peaks(sig)
     #if len(peaks):
