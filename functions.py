05bd17ad596800deff49d3a0c490523bb1fc34ba
mahdi <mahdi@adhawkmicrosystems.com>
2020-07-16 13:18:55 -0400

    new functions module for specific function implementations

---
+++ functions.py
@@ -0,0 +1,5 @@
+'''Typical function used in (statistical)(digital) signal processing'''
+
+def asinc(M):
+    '''The Aliasied Sinc function, defined as the sampled Rectanlge Window'''
+    count = M
