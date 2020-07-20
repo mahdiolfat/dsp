9fb892baac2865babd3c3691c40b51c662a22fc0
mahdi <mahdi@adhawkmicrosystems.com>
2020-07-19 20:19:52 -0400

    implemented aliased sinc function

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 '''Typical function used in (statistical)(digital) signal processing'''
 
-def asinc(M):
+import numpy as np
+
+def asinc(M, w):
     '''The Aliasied Sinc function, defined as the sampled Rectanlge Window'''
-    count = M
+    frange = np.arange(M)
+    return np.sin(frange * w / 2) / (frange * np.sin( w / 2))
