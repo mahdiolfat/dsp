495c2db80a798a684ab0049e936bf4be457762d3
mahdi <mahdi@adhawkmicrosystems.com>
2022-03-30 12:42:44 -0400

    added least square optimal fir filter. formatted util.

--- fir.py
+++ fir.py
@@ -3,9 +3,25 @@ import numpy as np
 def normalized_freq(freq):
     return 2 * np.pi * freq
 
-def lowpass_ideal(fc, fs=1, fcount=100):
-    np.full(())
-    np.ones((100))
+def lowpass_ideal(fc, taps, fs=1):
+    return 2 * fc / fs * np.sinc(2 * fc / fs * np.arange(taps))
+
+def spectrum_lowpass_ideal(fc, fs=1, fcount=None):
+    if fcount is None:
+        # scale such that cutoff frequency (fc) has an integer index
+        fcount = int(100 * fs / fc)
+
+    frange = np.linspace(0, fs, num = fcount)
+    fc_i = int(fc / fs * fcount)
+    filter = np.concatenate((np.ones((fc_i)), np.zeros((fcount - fc_i))), axis=None)
+    return frange, filter, fcount
+
+def lowpass_least_squares(fc, taps, fs=1):
+    '''
+        - due to energy Fourier energy theorem, the optimal least squares lowpass filter
+          is just the first L terms (# of taps) of the ideal lowpass filter (sinc function)
+    '''
+    return lowpass_ideal(fc, taps, fs=fs)
 
 def lowpass(freq_pb, ripple_pb, freq_sb, ripple_sb):
     '''
@@ -17,7 +33,21 @@ def lowpass(freq_pb, ripple_pb, freq_sb, ripple_sb):
             - transition width is 0, 
     '''
     transition_width = freq_sb - freq_pb
-    stopband_attenuation = 
+    stopband_attenuation = 20*np.log10(ripple_sb)
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    taps = 30
+    fc = 1/6
+    filter = lowpass_least_squares(fc, taps)
+    filter_spec = np.fft.rfft(filter, 1024)
+    plt.plot(np.abs(filter_spec))
+    #frange, filter, _ = spectrum_lowpass_ideal(1/4)
+    #plt.plot(frange, filter)
+    plt.figure()
+    lp = lowpass_ideal(fc, taps)
+    print(lp)
+    print(len(lp))
+    plt.bar(np.arange(taps), lp, width = 0.2)
+    #plt.bar(np.arange(taps), filter)
+    plt.show()
--- util.py
+++ util.py
@@ -5,29 +5,30 @@ import numpy as np
 def asinc(M, w):
     '''The Aliasied Sinc function, defined as the sampled Rectanlge Window.
        asinc becomes the sinc function in the limit as sampling rate -> inf'''
+    ''' handle the case where w = 0'''
     denom = w / 2 
     num = denom * M
     return np.sin(num) / (M * np.sin(denom))
 
 def fftshift(signal):
-   '''take fs/2 as negative frequency and dc (0) as positive to balance out
+    '''take fs/2 as negative frequency and dc (0) as positive to balance out
       frequency components'''
-   shifted = np.empty_like(signal)
-   midpoint = int(len(signal) / 2)
-   adj = 1 if len(signal) % 2 else 0
-   shifted[:midpoint] = signal[midpoint+adj:]
-   shifted[midpoint:] = signal[:midpoint+adj]
-   return shifted
+    shifted = np.empty_like(signal)
+    midpoint = int(len(signal) / 2)
+    adj = 1 if len(signal) % 2 else 0
+    shifted[:midpoint] = signal[midpoint+adj:]
+    shifted[midpoint:] = signal[:midpoint+adj]
+    return shifted
 
 def dft(signal):
-   '''
-      - discrete time
-      - finit frequency
-      - discrete frequency
-      - finit time
-   '''
+    '''
+        - discrete time
+        - finit frequency
+        - discrete frequency
+        - finit time
+    '''
 
-   wrange = np.arange(len(signal))
+    wrange = np.arange(len(signal))
 
 def bessel_first(x, terms=None):
-   return np.i0(x)
+    return np.i0(x)
